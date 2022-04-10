19fa27b7276756be272278a2825fde1a54ac5dcd
Rafikhan15 <mdrafi.khan0089@gmail.com>
2022-04-10 23:15:31 +0600

    Implemented login and redirect to shop

---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import './SignUp.css';
+import auth from '../../firebase.init'
+
+const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value)
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value)
+    }
+    const handleConfirmPasswordBlur = event => {
+        setConfirmPassword(event.target.value)
+    }
+
+    if (user) {
+        navigate('/shop');
+
+    }
+
+    const handleCreateUser = event => {
+        event.preventDefault();
+
+        if (password !== confirmPassword) {
+            setError('Your two password did not match');
+            return;
+        }
+        if (password.length < 6) {
+            setError('password must be 6 character or longer');
+            return;
+        }
+
+        createUserWithEmailAndPassword(email, password);
+    }
+
+    return (
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
+                <form onSubmit={handleCreateUser}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required />
+                    </div>
+                    <p style={{ color: 'red' }}>{error}</p>
+                    <input className='form-submit' type="submit" value="Sign Up" />
+                </form>
+                <p>
+                    Already Have an account? <Link className='form-link' to="/login">Login</Link>
+                </p>
+                <div className="line">
+                    <div className="first-line"></div>
+                    <span className=''>Or</span>
+                    <div className="first-line"></div>
+                </div>
+                <div>
+                    <input className='form-submit-with-google' type="submit" value="Continue With Google" />
+                    {/* <div className=''>
+                        <div><span>google</span></div>
+                        <div><h5>SignIn</h5></div>
+                    </div> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
